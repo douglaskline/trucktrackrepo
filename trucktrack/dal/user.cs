f462a7789379e6d2f479fbcc0a70c057c6768073
Doug Kline <douglaskline@gmail.com>
2022-01-25 13:09:15 -0500

    basic user class added

---
+++ trucktrack/dal/user.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace dal 
+{
+   public partial class User
+   {
+      public int Id {get; set;}
+      public string firstName {get; set;}
+      public string lastName {get; set;}
+
+      public string email {get; set;}
+   }
+}
