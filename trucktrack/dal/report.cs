2954228094144300b3aca56438afc5437cfce882
Doug Kline <douglaskline@gmail.com>
2022-02-08 13:46:40 -0500

    moved the user object to bottom of report class

--- trucktrack/dal/report.cs
+++ trucktrack/dal/report.cs
@@ -8,13 +8,13 @@ namespace dal
 
       public DateTime reportTime {get;set;}
 
-      public int userId {get; set;}
+
       public int truckId {get; set;}
       public int locationId {get; set;}
 
       public DateTime startTime {get;set;}
       public DateTime endTime {get;set;}
-
+      public int userId {get; set;}
       public dal.user user {get; set;}
    }
 }
