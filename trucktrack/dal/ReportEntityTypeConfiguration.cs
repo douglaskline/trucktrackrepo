e0bc76f4884141f512e5d281da00d8fac4c00a61
Doug Kline <douglaskline@gmail.com>
2022-02-08 13:52:07 -0500

    moved configuration to correct folder

--- trucktrack/dal/ReportEntityTypeConfiguration.cs
+++ trucktrack/dal/ReportEntityTypeConfiguration.cs
@@ -7,11 +7,21 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace dal
 {
 
-   public  class UserEntityTypeConfiguration : IEntityTypeConfiguration<dal.report>
+   public  class ReportEntityTypeConfiguration : IEntityTypeConfiguration<dal.report>
    {
       public void Configure(EntityTypeBuilder<dal.report> builder)
       {
+         builder.HasKey(r=>r.Id);
+
+         //builder.HasOne 
+         /*
          builder
+            .HasOne(r=r.userId)
+            .WithMany(r=r.)
+
+         builder
+            .HasOne(r=>r.userId)
+            .WithMany()
             .HasForeignKey<dal.user> (r=>r.userId);
 
          builder
@@ -34,26 +44,8 @@ namespace dal
             .Property(r=> r.locationId)
             .IsRequired();
 
-         /*
-         builder
-            .Property(u => u.Id)
-            .HasColumnName("Id")
-            .IsRequired();
 
-         builder
-            .Property(u => u.firstName);
-
-         builder
-            .Property(u => u.lastName)
-            .IsRequired();
-
-         builder
-            .Property(u => u.username)
-            .IsRequired();
-
-         builder
-            .Property(u => u.email)
-            .IsRequired();
+         
             */
       }
    }
