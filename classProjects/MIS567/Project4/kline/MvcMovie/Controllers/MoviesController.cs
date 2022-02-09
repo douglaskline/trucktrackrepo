d5d48d6e08b1361d8e580a3c57b9d282a58e1657
[Doug Kline] <[douglaskline@gmail.com]>
2022-02-09 14:37:59 -0500

    added genre search code

--- classProjects/MIS567/Project4/kline/MvcMovie/Controllers/MoviesController.cs
+++ classProjects/MIS567/Project4/kline/MvcMovie/Controllers/MoviesController.cs
@@ -19,18 +19,35 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
-        {
-            var movies = from m in _context.Movie
-                         select m;
+// GET: Movies
+public async Task<IActionResult> Index(string movieGenre, string searchString)
+{
+    // Use LINQ to get list of genres.
+    IQueryable<string> genreQuery = from m in _context.Movie
+                                    orderby m.Genre
+                                    select m.Genre;
 
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                movies = movies.Where(s => s.Title.Contains(searchString));
-            }
+    var movies = from m in _context.Movie
+                 select m;
 
-            return View(await movies.ToListAsync());
-        }
+    if (!string.IsNullOrEmpty(searchString))
+    {
+        movies = movies.Where(s => s.Title.Contains(searchString));
+    }
+
+    if (!string.IsNullOrEmpty(movieGenre))
+    {
+        movies = movies.Where(x => x.Genre == movieGenre);
+    }
+
+    var movieGenreVM = new MovieGenreViewModel
+    {
+        Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+        Movies = await movies.ToListAsync()
+    };
+
+    return View(movieGenreVM);
+}
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
