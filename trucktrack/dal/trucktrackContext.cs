8e2c0513c2f1a2ca5149069141be725bfc682fc9
[Aysun <[ak2756@uncw.edu]>
2022-02-10 16:40:20 -0500

    public virtual added DbSet <dal.truck> truck

--- trucktrack/dal/trucktrackContext.cs
+++ trucktrack/dal/trucktrackContext.cs
@@ -11,6 +11,7 @@ namespace dal
       public trucktrackContext(DbContextOptions<trucktrackContext> options) : base(options) { }
 
       public virtual DbSet<user> user { get; set; }
+      public virtual DbSet<dal.truck> truck { get; set; }
 
       protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
       {
