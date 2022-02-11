3247afa6fd32f65f8fa30788a23cd9e2676c520a
[LindsayKness] <[lk2255@uncw.edu]>
2022-02-10 21:30:59 -0500

    model

--- classProjects/MIS567/Project4/MvcMovie/Controllers/HelloWorldController.cs
+++ classProjects/MIS567/Project4/MvcMovie/Controllers/HelloWorldController.cs
@@ -5,20 +5,17 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
--- classProjects/MIS567/Project4/MvcMovie/Program.cs
+++ classProjects/MIS567/Project4/MvcMovie/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
