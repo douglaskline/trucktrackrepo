04a63ac32e75d236a4b852953b7b814dfc65dbf1
[James Dupree] <[dupreejames13@gmail.com]>
2022-02-17 11:21:01 -0500

    WORKING PART 4

---
+++ classProjects/MIS567/Project4/JamesDupree/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
