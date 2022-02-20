6435d650f070e0b9560f06460ef967a04810a744
[James Dupree] <[dupreejames13@gmail.com]>
2022-02-20 08:30:11 -0500

    Part 7(Search) working

--- classProjects/MIS567/Project4/JamesDupree/MvcMovie/Controllers/MoviesController.cs
+++ classProjects/MIS567/Project4/JamesDupree/MvcMovie/Controllers/MoviesController.cs
@@ -19,18 +19,34 @@ namespace MvcMovie.Controllers
             _context = context;
         }
 
-        // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+                        // GET: Movies// GET: Movies
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                         select m;
 
-            if (!String.IsNullOrEmpty(searchString))
+            if (!string.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
