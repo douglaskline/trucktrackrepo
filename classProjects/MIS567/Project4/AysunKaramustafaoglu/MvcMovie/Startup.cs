c538010be636d90d292e0f43b91085ea131463a5
[Aysun <[ak2756@uncw.edu]>
2022-02-24 12:33:15 -0500

    Part 4 - Use SQLite for development, SQL Server for production.
    I injected  IWebHostEnvironment into Startup.cs
    
    When SQLite is selected, the template generated code is ready for development. The following code shows how to inject IWebHostEnvironment into Startup.

--- classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Startup.cs
+++ classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Startup.cs
@@ -14,12 +14,15 @@ namespace MvcMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+        Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +30,22 @@ namespace MvcMovie
             services.AddControllersWithViews();
 
             services.AddDbContext<MvcMovieContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext")));
+            {
+                var connectionString = Configuration.GetConnectionString("MvcMovieContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });      
+        
+        
+        
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
