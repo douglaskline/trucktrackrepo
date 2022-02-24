376b8959f1bcd765c339c2d875f496c5d46d7552
[Aysun <[ak2756@uncw.edu]>
2022-02-24 13:37:05 -0500

    Part 5 - Add the seed initializer - in program.cs file, I changed the statement at the top using MvcMovie.Data; to using MvcMovie according to the Dr Kline instructions (he did that way)

--- classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Program.cs
+++ classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using MvcMovie.Data;
+using MvcMovie;
 using MvcMovie.Models;
 using System;
 
