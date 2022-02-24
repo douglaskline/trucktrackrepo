25049ed33ef7735d2b79312358da9d84283e2a0e
[Aysun <[ak2756@uncw.edu]>
2022-02-24 11:25:48 -0500

    Part 3 - In HelloWorldController.cs, change the Welcome method to add a Message and NumTimes value to the ViewData dictionary.

--- classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Controllers/HelloWorldController.cs
+++ classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Controllers/HelloWorldController.cs
@@ -5,21 +5,17 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
         public IActionResult Index()
         {
             return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
