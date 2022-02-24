c07ccd0c0ee7a3fc7a9969a7c63383c4f3e4c5a8
[Aysun <[ak2756@uncw.edu]>
2022-02-24 12:21:17 -0500

    Part 4 - Scaffold movie pages, Use the scaffolding tool to produce Create, Read, Update, and Delete (CRUD) pages for the movie model. I used the command (i am in windows machine) "dotnet-aspnet-codegenerator controller -name MoviesController -m Movie -dc MvcMovieContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -sqlite"

--- classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Startup.cs
+++ classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie
 {
@@ -24,6 +25,9 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
