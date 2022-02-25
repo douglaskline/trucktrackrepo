27e48eaba3463bdc5ad9293505b76c6a2a36bcef
[Doug Kline] <[douglaskline@gmail.com]>
2022-02-25 12:39:51 -0500

    added a couple of simple tests

--- trucktrack/daltests/UnitTest1.cs
+++ trucktrack/daltests/UnitTest1.cs
@@ -5,10 +5,21 @@ namespace daltests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+[Fact]
+        public void PassingTest()
         {
+            Assert.Equal(4, Add(2, 2));
+        }
 
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
