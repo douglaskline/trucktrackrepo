0c0df120494e93f5a1f64e7178b7396dc4979a18
[Doug Kline] <[douglaskline@gmail.com]>
2022-02-25 12:42:01 -0500

    added a theory example

--- trucktrack/daltests/UnitTest1.cs
+++ trucktrack/daltests/UnitTest1.cs
@@ -21,5 +21,21 @@ namespace daltests
         {
             return x + y;
         }
+
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
     }
 }
