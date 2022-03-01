3319daf55d03941134bdbe7d78f9fb5f016eeab8
[Aysun <[ak2756@uncw.edu]>
2022-03-01 12:50:47 -0500

    Part 8 - Update the SeedData class so that it provides a value for the new column. A sample change is shown in the code provided, but i made change  for each of the movie Movie.

--- classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Models/SeedData.cs
+++ classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Models/SeedData.cs
@@ -26,6 +26,7 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
@@ -34,6 +35,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -42,6 +44,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
                     },
 
@@ -50,6 +53,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
