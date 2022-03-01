f706315ce9dcc0f7b2304dd4ae820380ace972f1
[Aysun <[ak2756@uncw.edu]>
2022-03-01 12:07:25 -0500

    Part 7 - create a MovieGenreViewModel.cs file under models folder and Add the following MovieGenreViewModel class inside it

---
+++ classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
