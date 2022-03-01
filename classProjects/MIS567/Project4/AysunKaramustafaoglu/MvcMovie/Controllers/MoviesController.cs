e1382eddebf00f04f32c88ec836df9a522f23df0
[Aysun <[ak2756@uncw.edu]>
2022-03-01 11:52:11 -0500

    Part 7 - i previously changed the signature of the Index method to test how to pass the route-bound ID parameter, so now i change it back so that it takes a parameter named searchString as the code provided (because you can't expect users to modify the URL every time they want to search for a movie. So now you'll add UI elements to help them filter movies)

--- classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Controllers/MoviesController.cs
+++ classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Controllers/MoviesController.cs
@@ -19,18 +19,18 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string id)
+        public async Task<IActionResult> Index(string searchString)
         {
             var movies = from m in _context.Movie
                         select m;
 
-            if (!String.IsNullOrEmpty(id))
+            if (!String.IsNullOrEmpty(searchString))
         {
-            movies = movies.Where(s => s.Title.Contains(id));
+            movies = movies.Where(s => s.Title.Contains(searchString));
         }
 
-            return View(await movies.ToListAsync());
-        }
+    return View(await movies.ToListAsync());
+}
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
         {
