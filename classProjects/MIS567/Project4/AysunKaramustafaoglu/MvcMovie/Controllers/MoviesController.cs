ea0494d467fa48668455b582e08fa96a42d9b352
[Aysun <[ak2756@uncw.edu]>
2022-03-01 12:44:04 -0500

    Part 8 - In MoviesController.cs, update the [Bind] attribute for both the Create and Edit action methods to include the Rating property (Because i've added a new field to the Movie class, you need to update the property binding list so this new property will be included)

--- classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Controllers/MoviesController.cs
+++ classProjects/MIS567/Project4/AysunKaramustafaoglu/MvcMovie/Controllers/MoviesController.cs
@@ -77,7 +77,7 @@ public async Task<IActionResult> Index(string movieGenre, string searchString)
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -109,7 +109,7 @@ public async Task<IActionResult> Index(string movieGenre, string searchString)
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
             {
