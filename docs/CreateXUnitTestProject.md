8907c3b30d36fddb6dfa4039d7f0c1c36cab05ad
Doug Kline <douglaskline@gmail.com>
2022-03-01 13:22:35 -0500

    basic distance calculator working with test

--- docs/CreateXUnitTestProject.md
+++ docs/CreateXUnitTestProject.md
@@ -2,6 +2,7 @@ Commands I used to create the utiltests project
 ==================================================
 
 from the trucktrack root folder where the .sln file is
+
 ```shell
 dotnet new xunit -o utiltests -f net5.0 
 cd utiltests
---
+++ trucktrack/utiltests/utilTest1.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+using util;
+
+namespace utiltests
+{
+    public class utilTest1
+    {
+        [Fact]
+        public void DistanceCalculatorConstructorWorks()
+        {
+            // arrange
+            util.DistanceCalculator d = new DistanceCalculator();
+
+            // act
+
+            // assert
+          Assert.IsType<util.DistanceCalculator>(d);       
+        }
+
+        [Fact]
+         public void DistanceCalculatorEuclideanDistance()
+        {
+            // arrange
+            
+            double xLat = 34.22697;
+            double xLong = -77.86989;
+            double yLat = 34.24539;
+            double yLong = -77.95102;
+            // act
+            double expected = 4.802481904079362;
+            double actual = DistanceCalculator.EuclideanDistance(xLat,xLong,yLat,yLong);
+            
+            // assert
+          Xunit.Assert.Equal(expected,actual);
+          
+          
+        }
+
+    }
+}
