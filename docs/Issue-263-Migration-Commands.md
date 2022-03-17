a16dda9836d18b93719af7fe987e1aac1f38d284
nischalsaladi <61411820+nischalsaladi@users.noreply.github.com>
2022-03-17 11:59:12 -0400

    Migration Milestone0.2

--- docs/Issue-263-Migration-Commands.md
+++ docs/Issue-263-Migration-Commands.md
@@ -1,4 +1,6 @@
+#SCAFFOLD 
 Get into the ui folder before issue the each commands
+
 USER
 dotnet aspnet-codegenerator controller -name userController -async -m dal.user -dc dal.trucktrackContext -udl --relativeFolderPath Controllers --referenceScriptLibraries
 
@@ -11,3 +13,25 @@ dotnet aspnet-codegenerator controller -name locationController -async -m dal.lo
 
 REPORT
 dotnet aspnet-codegenerator controller -name reportController -async -m dal.report -dc dal.trucktrackContext -udl --relativeFolderPath Controllers --referenceScriptLibraries
+
+#CREATE A MIGRATION
+Get into the dal folder before issue the each commands(The connection code is hard coded in the trucktrackfactory)
+
+```shell
+% cd dal
+# make sure you are in the dal project folder
+
+dotnet ef migrations add Milestone02 -c trucktrackContext -s ../ui/ui.csproj
+
+```
+
+The command above should create some files under the /trucktrack/dal/Migrations folder.
+
+## Create a sql script from the migration
+
+```shell
+% cd Migrations
+# make sure you are in the /trucktrack/dal/Migrations folder
+
+dotnet ef migrations script 0  InitialMigration -o dbmigration.sql -- connectionstring
+```
---
+++ trucktrack/dal/Migrations/20220317155705_Milestone02.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dal.Migrations
+{
+    public partial class Milestone02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "truckURL",
+                table: "truck",
+                type: "VARCHAR(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "truckName",
+                table: "truck",
+                type: "VARCHAR(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "tagLine",
+                table: "truck",
+                type: "VARCHAR(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "priceRange",
+                table: "truck",
+                type: "VARCHAR(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "homeState",
+                table: "truck",
+                type: "CHAR(2)",
+                maxLength: 2,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "homeCity",
+                table: "truck",
+                type: "VARCHAR(75)",
+                maxLength: 75,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "foodStyle",
+                table: "truck",
+                type: "VARCHAR(75)",
+                maxLength: 75,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "locationDescription",
+                table: "location",
+                type: "VARCHAR(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "truckURL",
+                table: "truck",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "truckName",
+                table: "truck",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "tagLine",
+                table: "truck",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "priceRange",
+                table: "truck",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "homeState",
+                table: "truck",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "CHAR(2)",
+                oldMaxLength: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "homeCity",
+                table: "truck",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(75)",
+                oldMaxLength: 75,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "foodStyle",
+                table: "truck",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(75)",
+                oldMaxLength: 75,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "locationDescription",
+                table: "location",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
