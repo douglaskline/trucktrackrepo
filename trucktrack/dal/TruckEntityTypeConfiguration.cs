bcc880e4324545ba6e3756d1e78022923359e438
nischalsaladi <61411820+nischalsaladi@users.noreply.github.com>
2022-03-17 11:02:35 -0400

    Added colume type for truck

--- trucktrack/dal/TruckEntityTypeConfiguration.cs
+++ trucktrack/dal/TruckEntityTypeConfiguration.cs
@@ -20,25 +20,39 @@ namespace dal
 
          builder
             .Property(t => t.truckName)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(200)
             .IsRequired();
 
          builder
-            .Property(t => t.homeCity);
+            .Property(t => t.homeCity)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(75);
 
          builder
-            .Property(t => t.homeState);
+            .Property(t => t.homeState)
+            .HasColumnType("CHAR")
+            .HasMaxLength(2);
 
          builder
-            .Property(t => t.foodStyle);
+            .Property(t => t.foodStyle)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(75);
 
          builder
-            .Property(t => t.priceRange);
+            .Property(t => t.priceRange)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(10);
 
          builder
-            .Property(t => t.tagLine);
+            .Property(t => t.tagLine)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(200);
 
          builder
-            .Property(t => t.truckURL);
+            .Property(t => t.truckURL)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(300);
 
          builder.Property(t => t.timeStamp).IsRowVersion();
 
