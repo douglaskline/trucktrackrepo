98673fd4c83826ddf5995b69402e09bc89cee663
[ethancook6151] <[etc6151uncw@gmail.com]>
2022-03-19 20:27:59 -0400

    Added Controllers and blank views

---
+++ trucktrack/ui/Controllers/findController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using dal;
+
+namespace ui.Controllers
+{
+    public class findController : Controller
+    {
+        private readonly trucktrackContext _context;
+
+        public findController(trucktrackContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ trucktrack/ui/Controllers/nearbyController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using dal;
+
+namespace ui.Controllers
+{
+    public class nearbyController : Controller
+    {
+        private readonly trucktrackContext _context;
+
+        public nearbyController(trucktrackContext context)
+        {
+            _context = context;
+        }
+    }
+}
