a6f3a34d405f2c4db87df57e1c0f782883a936f1
CHRIS_HENSLEY <cdh5809@uncw.edu>
2022-03-31 16:45:05 -0400

    Changes to Project 13

---
+++ trucktrack/utiltests/HensleyProj13.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+using util;
+
+namespace utiltests
+{
+    public class utilTest1
+    {
+        [Fact]
+        public void DistanceCalculatorConstructorWorks()
+        {
+            // arrange
+            util.DistanceCalculator d = new DistanceCalculator();
+
+            // act
+
+            // assert
+          Assert.IsType<util.DistanceCalculator>(d);       
+        }
+
+        [Fact]
+         public void DistanceCalculatorEuclideanDistance()
+        {
+            // arrange
+            
+            double xLat = 24.547247406217483;
+            double xLong = -81.78725284822347;
+            double yLat = 34.22282496667014;
+            double yLong = -77.86343988551839;
+            // act
+            double expected = 4.802481904079362;
+            double actual = DistanceCalculator.EuclideanDistance(xLat,xLong,yLat,yLong);
+            
+            // assert
+            double difference = Math.Abs(expected - actual);
+
+          Xunit.Assert.True (difference <= 0.25);
+           "actual: " + actual.ToString() + " expected: " + expected.ToString() + " diff: " + difference.ToString();
+          
+        }
+
+    }
+}
