e7e241726fb49a295569bd933c327028e7efc740
Aubrey Bordeaux <tbordeaux1@yahoo.com>
2022-03-31 15:10:50 -0400

    added failed error message

--- trucktrack/utiltests/BordeauxProj13.cs
+++ trucktrack/utiltests/BordeauxProj13.cs
@@ -23,18 +23,19 @@ namespace utiltests
         {
             // arrange
             
-            double xLat = 28.37820;
-            double xLong = -81.57040;
-            double yLat = 36.21718;
-            double yLong = -86.69049;
+            double xLat = 36.20710;
+            double xLong = -86.69208;
+            double yLat = 28.37832;
+            double yLong = -81.57010;
             // act
-            double expected = 4.802481904079362;
+            double expected = 618;
             double actual = DistanceCalculator.EuclideanDistance(xLat,xLong,yLat,yLong);
             
             // assert
             double difference = Math.Abs(expected - actual);
 
-          Xunit.Assert.True (difference <= 0.0001);
+          Xunit.Assert.True (difference <= 2,
+                  "actual: " + actual.ToString() + " expected: " + expected.ToString() +" diff: " + difference.ToString());
           
           
         }
