d59338d59db7a7af64a1af80ca9b89a93df821dc
noah-vu <nv8663@uncw.edu>
2022-03-31 16:00:52 -0400

    Distance from RL to MSG

---
+++ trucktrack/utiltests/VuProj13.cs
@@ -0,0 +1,44 @@
+using System; 
+using Xunit;
+using util;
+
+namespace utiltests
+{
+    public class VuProj13
+    {
+        [Fact]
+        public void DistanceCalculatorConstructorWorks()
+        {
+            // arrange
+            util.DistanceCalculator d = new DistanceCalculator();
+
+            // act
+
+            // assert
+          Assert.IsType<util.DistanceCalculator>(d);       
+        }
+
+        [Fact]
+         public void RandallLibraryToMadisonSquareGardenTest()
+        {
+            // arrange
+            
+            double RandallLibraryLat = 34.22799004747062;
+            double RandallLibraryLong = -77.87426608846991;
+            double MadisonSquareGardenLat = 40.75074018304554;
+            double MadisonSquareGardenLong = -73.99343870183384;
+            // act
+            double expected = 498.23;
+            double actual = DistanceCalculator.EuclideanDistance(RandallLibraryLat,RandallLibraryLong,MadisonSquareGardenLat,MadisonSquareGardenLong);
+            
+            // assert
+            double difference = Math.Abs(expected - actual);
+
+          Xunit.Assert.True (difference <= 0.25);
+          
+          
+        }
+
+    }
+}
+
