e3cd1e9a34e3fed5019ed1d8488207bf36c41ea9
Doug Kline <douglaskline@gmail.com>
2022-04-05 09:57:37 -0400

    setup of the routingTests factory

--- trucktrack/uitests/RoutingTests.cs
+++ trucktrack/uitests/RoutingTests.cs
@@ -5,7 +5,15 @@ using Xunit;
 namespace uitests
 {
     public class RoutingTests
+        :IClassFixture<WebApplicationFactory<ui.Startup>>
     {
+        private readonly WebApplicationFactory<ui.Startup> _factory;
+
+        public RoutingTests(WebApplicationFactory<ui.Startup> factory)
+        {
+            _factory = factory;
+        }
+
         [Fact]
         public void Test1()
         {
