af5caf390b42bfd2740611ae355e7e53b6978e1b
[Aysun <[ak2756@uncw.edu]>
2022-04-05 17:02:35 -0400

    line 38 was the part of line 37, on line 37 i deleted the ; sign

--- trucktrack/utiltests/AysunProj9.cs
+++ trucktrack/utiltests/AysunProj9.cs
@@ -34,8 +34,8 @@ namespace utiltests
             // assert
             double difference = Math.Abs(expected - actual);
 
-          Xunit.Assert.True(difference <= 0.25);
-                       "actual: " + actual.ToString() + " expected: " + expected.ToString() + " diff: " + difference.ToString();
+          Xunit.Assert.True(difference <= 0.25, 
+          "actual: " + actual.ToString() + " expected: " + expected.ToString() + " diff: " + difference.ToString());
           
         }
 
