cec56f7ba6d8d5191b8e3c1af148294609d9883e
[Aysun <[ak2756@uncw.edu]>
2022-04-06 18:34:04 -0400

    i added 3 truck details inside the truck.sql file

---
+++ trucktrack/dal/Migrations/truck.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO Truck (truckName, homecity, homestate, foodstyle, pricerange, tagline, truckURL) 
+
+VALUES
+
+('Poor Piggy''s BBQ & Catering', 'Wilmington', 'NC', 'BBQ', '$', 'Wilmington''s First and Favorite Food Trucks!', 'https://www.poorpiggys.com/piggy-truck'), 
+
+('Wilmiricans', 'Wilmington', 'NC', 'Puerto Rican', '$', '', ''), 
+
+('ForTheShellOfIt', 'Wilmington', 'NC', 'Seafood', '$$', 'BRINGING NEW ENGLAND TO COASTAL NC!', 'https://shellofit.com/')
+
+ 
