46bdc8911ec02f0b10b1ceaedde1775a3107d69d
nischalsaladi <61411820+nischalsaladi@users.noreply.github.com>
2022-04-14 21:12:30 -0400

    changes made to lat long

--- trucktrack/dal/Migrations/location.sql
+++ trucktrack/dal/Migrations/location.sql
@@ -1,13 +1,13 @@
 INSERT INTO Location (locationName, latitude, longitude) 
 VALUES
-('The Rockin'' Roller Salon','34.2169759357414','-77.886522701949'), 
-('Advanced Auto Parts','34.1887020034368','-77.9333971712566'), 
-('Broomtail Craft Brewery','34.2597662984362','-77.8467037307762'), 
-('New Hanover County Courthouse','34.2365333364761','-77.9451983442696'), 
-('Mad Mole Brewing','34.2124232545099','-77.8343470307771'), 
-('Flytrap Brewing','34.2405550062868','-77.945445528927'), 
-('Waterline Brewing Company','34.2257545696708','-77.9483469712559'), 
-('Leaf and Barrel Provision Co. Beer/Wine/Cigars','34.3706301042416','-77.7067775442673'), 
-('Outer Dunes Brewing Company','34.2912434167404','-77.7992114731044'), 
-('Flying Machine Brewing Company','34.229737431361','-77.9011308461194'), 
-('Jerry''s Barber Shop','34.3869263967874','-77.8945617289244')
+('The Rockin'' Roller Salon',34.2169759357414,-77.886522701949),
+('Advanced Auto Parts',34.1887020034368,-77.9333971712566),
+('Broomtail Craft Brewery',34.2597662984362,-77.8467037307762),
+('New Hanover County Courthouse',34.2365333364761,-77.9451983442696),
+('Mad Mole Brewing',34.2124232545099,-77.8343470307771),
+('Flytrap Brewing',34.2405550062868,-77.945445528927),
+('Waterline Brewing Company',34.2257545696708,-77.9483469712559),
+('Leaf and Barrel Provision Co. Beer/Wine/Cigars',34.3706301042416,-77.7067775442673),
+('Outer Dunes Brewing Company',34.2912434167404,-77.7992114731044),
+('Flying Machine Brewing Company',34.229737431361,-77.9011308461194),
+('Jerry''s Barber Shop',34.3869263967874,-77.8945617289244)
