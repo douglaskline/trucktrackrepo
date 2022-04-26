11c7a5ff963574ac8789ca2878c1a790c120ca55
[JD Sturdivant] <[jds6589@uncw.edu]>
2022-04-26 15:51:58 -0400

    Added lat longs to end of url on a anchor

--- trucktrack/ui/wwwroot/js/site.js
+++ trucktrack/ui/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var coords = {lat: "", lon: ""};
+
+var x = document.getElementById("location");     
+       function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+        } else { 
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+      }
+      
+      function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude + 
+        "<br>Longitude: " + position.coords.longitude;
+        coords.lat = position.coords.latitude;
+        coords.lon = position.coords.longitude;
+        window.alert(coords.lat);
+        return [position.coords.latitude,position.coords.longitude]
+      }
+var jsarray=  navigator.geolocation.getCurrentPosition(showPosition)
+sessionStorage.setItem("jsArray", JSON.stringify(jsarray));
+
+//window.location = window.location + '/' + jsarray[0] + '/' + jsarray[1];
+  $('a.redirect').click(function (event) {
+  getLocation()
+  var href = $(this).attr('href')
+  var loc = 'https://localhost:5001'+ href + '/' + coords.lat + '/';
+  var loc = loc + coords.lon;
+  event.preventDefault();
+  var href = $(this).attr('href')
+  window.location=loc;
+});
