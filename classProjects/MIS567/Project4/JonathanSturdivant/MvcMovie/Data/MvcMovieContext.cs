24d4d02edee0f4dce5f040394af47ea46c3a9c6f
[JD Sturdivant] <[jds6589@uncw.edu]>
2022-05-04 10:54:02 -0400

    Moved project from local machine to github

---
+++ classProjects/MIS567/Project4/JonathanSturdivant/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,17 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
